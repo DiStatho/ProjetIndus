c37f17903b11bf24ca7abc21dd0f104ee7f1f817
BuildTools <jonathan.navarro@etu.univ-amu.fr>
2017-02-02 17:39:07 +0100

    First Commit
    Roll A Ball : Amazing you collected the dick !

---
+++ SwBattleShip/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+    private string LaPutainDeTaMere;
+
+    void Start () {
+        LaPutainDeTaMere = "SOUETANE A MOUK !";
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Debug.Log(LaPutainDeTaMere);
+	}
+}

5aea86fa420d2a9f7779ecfba732b2e9484ca88b
Shuko83 <digrande.fabien@gmail.com>
2017-02-23 15:58:05 +0100

    Ajout du stockage de la variable son et musique
    Quand l’utilisateur change le toge bouton en on ou off, la donnée est
    sauvegardée. Lorsque celui ci quitte le menu est revient sur la page du
    menu les variables sont conservées.

---
+++ SwBattleShip/Assets/Scripts/Settings/Setting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Setting : MonoBehaviour {
+
+	public Toggle toggleSound, toggleMusic;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void ChangeStateMusic(){
+		GlobalData.SetMusicActive (toggleMusic.isOn);			
+	}
+	public void ChangeStateSound(){
+		GlobalData.SetSoundActive (toggleSound.isOn);
+	}
+
+	public void ChangeLangage(){
+		
+	}
+
+	
+
+}
